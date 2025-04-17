95043c4072cb7c9ef716b5b1d5e47da857bddd1e
ljp <2540297235@qq>
2025-04-17 18:29:42 +0800

    111

---
+++ Assets/Client/Client_Base/ChronosSystem/ChronosSystem.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class ChronosSystem : SingletonMono<ChronosSystem>
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        InitTimeScales();
+    }
+
+    private void InitTimeScales()
+    {
+        
+    }
+}
---
+++ Assets/Client/Client_Base/ChronosSystem/LocalTimeScale.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TimeScale", menuName = "Chronos/TimeScale")]
+public class LocalTimeScale : ScriptableObject
+{
+    [SerializeField]
+    protected float scaleValue = 1f;
+    [SerializeField]
+    protected bool autoRemoveWhenEmpty = true;
+
+    protected float lastValue = 1f;
+    protected bool isPaused = false;
+    protected readonly HashSet<ILocalTimer> localTimeContainer = new();
+
+    public bool AutoRemoveWhenEmpty
+    {
+        get => autoRemoveWhenEmpty;
+        set => autoRemoveWhenEmpty = value;
+    }
+
+    public bool IsPaused => isPaused;
+    public string[] LocalTimerNames => localTimeContainer.Select(s => s.ToString()).ToArray();
+    public GameObject[] LocalTimerObjects => localTimeContainer.Select(s => s.GameObject).ToArray();
+    public float ScaleValue
+    {
+        get => scaleValue;
+        set => scaleValue = value;
+    }
+
+}
