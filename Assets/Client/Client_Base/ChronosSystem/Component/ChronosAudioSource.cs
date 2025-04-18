c6924d456737f35c4f7e9dc8a6ec7f2515ecb62d
ljp <2540297235@qq>
2025-04-18 18:38:40 +0800

    111

---
+++ Assets/Client/Client_Base/ChronosSystem/Component/ChronosAudioSource.cs
@@ -0,0 +1,39 @@
+﻿
+using UnityEngine;
+
+[System.Serializable]
+public class ChronosAudioSource : ChronosMonoBehaviour
+{
+    public ChronosComponent chronosComponent;
+    public AudioSource[] audioSources;
+
+    private float[] cachedPitched;
+    protected float cachedTimeScale;
+
+    public float minTimeScale = 0f;
+    public float maxTimeScale = 2f;
+    public float minPitch = 0.5f;
+    public float maxPitch = 1.5f;
+
+    protected virtual void Awake()
+    {
+        OnValidate();
+    }
+
+    protected virtual void Update()
+    {
+        if (Mathf.Approximately(chronosComponent.TimeScale, cachedTimeScale))
+            return;
+
+        cachedTimeScale = chronosComponent.TimeScale;
+    }
+
+    protected virtual void OnValidate()
+    {
+        if (audioSources == null || audioSources.Length == 0)
+            audioSources = GetComponentsInChildren<AudioSource>();
+
+        if (chronosComponent == null)
+            chronosComponent = GetComponent<ChronosComponent>();
+    }
+}
