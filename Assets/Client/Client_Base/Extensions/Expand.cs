a8c8e1b40935adfa8f11fa040ce843b5f8dc0976
ljp <2540297235@qq>
2025-05-08 18:22:05 +0800

    指令功能

--- Assets/Client/Client_Base/Extensions/Expand.cs
+++ Assets/Client/Client_Base/Extensions/Expand.cs
@@ -2,9 +2,14 @@
 
 using System;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 public static class Expand
 {
+    public static void ThrowException(this string reason)
+    {
+        throw new System.Exception("ET stop running because " + reason);
+    }
 
     public static void Reset(this Transform ts)
     {
@@ -42,5 +47,41 @@ public static class Expand
         }
 
         return component;
+    }
+
+    public static Transform FindNode(this Transform t, params string[] nodes)
+    {
+        var curt = t;
+        for (int i = 0; i < nodes.Length; i++)
+        {
+            string node = nodes[i];
+            curt = curt.Find(node);
+            if (curt == null) $"The node '{node}' is not found".ThrowException();
+        }
+        return curt;
+    }
+
+    public static T FindComponent<T>(this Transform t, params string[] nodes) where T : Component
+    {
+        var child = t.FindNode(nodes);
+        if (child.TryGetComponent<T>(out var component)) return component;
+        return child.gameObject.AddComponent<T>();
+    }
+
+    public static T ClearChildrenExceptFirst<T>(this Transform t) where T : Component
+    {
+        var children = new Transform[t.childCount];
+        for (var i = 0; i < t.childCount; i++)
+            children[i] = t.GetChild(i);
+
+        for (var i = 1; i < children.Length; i++)
+        {
+            children[i].gameObject.SetActive(false);
+            Object.Destroy(children[i].gameObject);
+        }
+
+        var child = children[0];
+        child.gameObject.SetActive(false);
+        return child.FindComponent<T>();
     }
 }
